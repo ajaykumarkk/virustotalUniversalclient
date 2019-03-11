1c522c031e67e8e2e185347f519b5ba232b3156d
ajaykumarkk <ajaykumarkk77@gmail.com>
2019-03-11 15:38:24 +0530

    minor changes

--- run.py
+++ run.py
@@ -5,11 +5,10 @@ with open('src//apikeys.txt', 'r') as f:
 	apikeys = [line.rstrip('\n') for line in open('src//apikeys.txt')]
 
 files_path = [os.path.abspath("file_upload\\"+x) for x in os.listdir("file_upload")]
-exes_lst=[]
+files={}
 for i in files_path:
 	filename,ext = os.path.splitext(i)
 	if ext == ".exe":
 		if os.path.getsize(i) < 32000000:
-			exes_lst.append(i)
-
-print(exes_lst)
+			files[i]=md5Checksum(i)
+print(files)
